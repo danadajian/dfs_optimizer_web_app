22d06247115092a6dc3db179b2806f89bb712e52
dadajian <danadajian@gmail.com>
2020-05-26 15:27:24 -0400

    * blob method

--- src/handlers/handleExportLineup/handleExportLineup.ts
+++ src/handlers/handleExportLineup/handleExportLineup.ts
@@ -10,7 +10,17 @@ export const handleExportLineup = async (navigator: any, componentRef: any) => {
     }).then((canvas: HTMLCanvasElement) => {
         return canvas.toDataURL('image/png', 1.0)
     }).then((dataUrl: string) => {
-        const newTab = window.open()!;
-        newTab.document.body.innerHTML = `<img src=${dataUrl} alt="lineup">`;
+        // const newTab = window.open()!;
+        // newTab.document.body.innerHTML = `<img src=${dataUrl} alt="lineup">`;
+
+        let byteCharacters = atob(dataUrl.split('data:image/png;base64,')[1]);
+        let byteNumbers = new Array(byteCharacters.length);
+        for (let i = 0; i < byteCharacters.length; i++) {
+            byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        let byteArray = new Uint8Array(byteNumbers);
+        let file = new Blob([byteArray], { type: 'image/png' + ';base64' });
+        let fileURL = URL.createObjectURL(file);
+        window.open(fileURL);
     })
 }
