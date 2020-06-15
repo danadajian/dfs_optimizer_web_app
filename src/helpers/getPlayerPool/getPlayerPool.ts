b8a32f846c3342c88921be498c742956e0e37112
Dan Adajian <danadajian@gmail.com>
2020-06-15 10:24:03 -0500

    * playerpool tweak

--- src/helpers/getPlayerPool/getPlayerPool.ts
+++ src/helpers/getPlayerPool/getPlayerPool.ts
@@ -8,26 +8,24 @@ export const getPlayerPool = (dfsPlayers: any[], projectionsData: any, playerHis
                 .find(playerId => projectionsData[playerId].name === player.name)!);
         }
         return projectionsData[player.playerId];
-    }).map((player: any) => {
+    }).map(player => {
         const playerData = projectionsData[player.playerId];
-        if (playerData) {
-            const opposingTeam = playerData.opponent.split(' ')[1];
-            const teamRanks = opponentRanks[TEAM_ABBREVIATIONS[opposingTeam]];
-            const playerHistoryPlayer = playerHistory.find((playerObject: any) => playerObject.playerId === player.playerId);
-            const opponentRankPosition: any = Object.keys(teamRanks)
-                .find(position => position.replace('/', '').includes(player.position));
-            const playerStatus = playerStatuses.find((player: any) => player.name === playerData.name) ?
-                playerStatuses.find((player: any) => player.name === playerData.name).status : '';
-            const injuryStatus = injuries[playerData.name] ? injuries[playerData.name].toLowerCase() : '';
-            return {
-                ...player,
-                ...playerData,
-                projection: playerData[site + 'Projection'],
-                rollingAverage: playerHistoryPlayer && playerHistoryPlayer[site],
-                opponentRank: teamRanks[opponentRankPosition],
-                status: (playerStatus || injuryStatus) &&
-                    `${INJURY_ABBREVIATIONS[injuryStatus] || ''}${playerStatus ? ' ' : ''}${playerStatus}`
-            };
-        }
+        const opposingTeam = playerData.opponent.split(' ')[1];
+        const teamRanks = opponentRanks[TEAM_ABBREVIATIONS[opposingTeam]];
+        const playerHistoryPlayer = playerHistory.find((playerObject: any) => playerObject.playerId === player.playerId);
+        const opponentRankPosition: any = Object.keys(teamRanks)
+            .find(position => position.replace('/', '').includes(player.position));
+        const playerStatus = playerStatuses.find((player: any) => player.name === playerData.name) ?
+            playerStatuses.find((player: any) => player.name === playerData.name).status : '';
+        const injuryStatus = injuries[playerData.name] ? injuries[playerData.name].toLowerCase() : '';
+        return {
+            ...player,
+            ...playerData,
+            projection: playerData[site + 'Projection'],
+            rollingAverage: playerHistoryPlayer && playerHistoryPlayer[site],
+            opponentRank: teamRanks[opponentRankPosition],
+            status: (playerStatus || injuryStatus) &&
+                `${INJURY_ABBREVIATIONS[injuryStatus] || ''}${playerStatus ? ' ' : ''}${playerStatus}`
+        };
     });
 };
